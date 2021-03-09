ba9bc64e4db4b0b3188fea669dccca5749c3a501
Sho50302 <61681052+Sho50302@users.noreply.github.com>
2021-03-09 14:13:16 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,13 +1,27 @@
 import _ from 'lodash';
-import { READ_EVENTS } from '../actions';
+import {
+  READ_EVENTS,
+  READ_EVENT,
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+} from '../actions';
 
 /* export defaultの時点で変数宣言しているため、
 export defaultの後に関数を宣言するとWarningが出る。
 そのため一時的に関数を変数に格納し、exportする */
 const switchEvents = (events = {}, action) => {
   switch (action.type) {
+    case READ_EVENT:
+    case CREATE_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data;
+      return { ...events, [data.id]: data }
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id');
+    case DELETE_EVENT:
+      delete events[action.id];
+      return { ...events };
     default:
       return events;
   }
